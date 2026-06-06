7e005c3b4f2b87375897a6bdd9f59eb1b6ec694d
Yoann <121693675+F4JTV@users.noreply.github.com>
2026-06-06 14:59:15 +0200

    Update git clone URLs for mbelib and dsd-fme

--- download-and-install-ubuntu2404lts.sh
+++ download-and-install-ubuntu2404lts.sh
@@ -2,7 +2,7 @@
 #
 cdir=$(pwd)
 sudo apt install libpulse-dev libsndfile1-dev libfftw3-dev liblapack-dev socat libusb-1.0-0-dev rtl-sdr librtlsdr-dev libusb-1.0-0-dev cmake git wget make build-essential libncurses-dev libncurses6 libcodec2-dev
-git clone https://github.com/lwvmobile/mbelib
+git clone https://github.com/F4JTV/mbelib
 cd mbelib
 git checkout ambe_tones
 mkdir build
@@ -12,7 +12,7 @@ make -j $(nproc)
 sudo make install
 sudo ldconfig
 cd $cdir
-git clone https://github.com/lwvmobile/dsd-fme
+git clone https://github.com/F4JTV/dsd-fme
 cd dsd-fme
 #git checkout audio_work
 mkdir build
